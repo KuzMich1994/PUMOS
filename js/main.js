e4e1806890152266054c5eb8374f3470dadc79d9
KuzMich1994 <drumer19940310@mail.ru>
2020-11-05 01:41:12 +0300

    refact: changed settings and styles to modal window

--- js/main.js
+++ js/main.js
@@ -5,20 +5,32 @@ menuButton.addEventListener('click', function() {
   document.querySelector('body').classList.toggle('scroll-hidden')
 })
 
-let modalButton = $('[data-toggle="order"]')
-let modalButtonClose = $('.modal__button-close')
+let modalButton = $('[data-toggle="order"]');
+let modalButtonClose = $('.modal__button-close');
+let modalClose = $('.modal__overlay');
 modalButton.on('click', openModal);
 modalButtonClose.on('click', closeModal);
+modalClose.on('click', closeModal);
 
 function openModal() {
   let modalOverlay = $('.modal__overlay')
+  let modalDialog = $('.modal__dialog')
   modalOverlay.addClass('modal__overlay_visible')
+  modalDialog.addClass('modal__dialog_visible')
   $('body').addClass('scroll-hidden')
 }
 
 function closeModal() {
   let modalOverlay = $('.modal__overlay')
+  let modalDialog = $('.modal__dialog')
   modalOverlay.removeClass('modal__overlay_visible')
+  modalDialog.removeClass('modal__dialog_visible')
   $('body').removeClass('scroll-hidden')
 }
+$(document).click(function(e) {
+  if($(e.target).is('.modal__overlay')) {
+    closeModal();
+  }
+})
+
 })
