ec7bd7a7c9de0f33b7840afa9ca7d70d0d297b50
KuzMich1994 <drumer19940310@mail.ru>
2020-12-19 19:37:08 +0300

    Поменял фотографии, заменил ссылки под свою папку на хостинге

--- send.php
+++ send.php
@@ -5,25 +5,69 @@ require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
 // Переменные, которые отправляет пользователь
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$name = $_POST['name'];
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+$name = isset($_POST['name']) ? $_POST['name'] : '';
 
 // Формирование самого письма
 if (empty($email)) {$title = "Консультация";
 $body = "
 <h2>Новое письмо</h2>
-<b>Имя:</b><br><br>$name
-<b>Телефон:</b><br>$phone
+<b>Имя: </b>$name<br><br>
+<b>Телефон: </b>$phone
 ";} else {
   $title = "Консультация";
 $body = "
 <h2>Новое письмо</h2>
-<b>Имя:</b>$name
-<b>E-mail:</b><br><br>$email
-<b>Телефон:</b><br><br>$phone
+<b>Имя: </b>$name<br><br>
+<b>E-mail: </b>$email<br><br>
+<b>Телефон: </b>$phone
 ";
 }
+
+
+
+
+
+
+
+$c = true;
+$message = '';
+
+foreach ( $_POST as $key => $value ) {
+  if ( $value != "" ) {
+
+    if ($value === 'on') {
+      $value = 'Да';
+    }
+
+    if ($key === 'counter') {
+      $key = 'Добавить счетчик в комплект';
+    }
+
+    if ($key === 'stand') {
+      $key = 'Добавить стойку в комплект';
+    }
+
+    $message .= "
+    " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+    </tr>
+    ";
+  }
+}
+
+$message = "<table style='width: 100%;'>$message</table>";
+
+
+
+
+
+
+
+
+
 
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
@@ -35,20 +79,21 @@ try {
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
     // Настройки вашей почты
-    $mail->Host       = ''; // SMTP сервера вашей почты
-    $mail->Username   = ''; // Логин на почте
-    $mail->Password   = ''; // Пароль на почте
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'sergey.student1994@mail.ru'; // Логин на почте
+    $mail->Password   = 'web50607011'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
-    $mail->setFrom(''); // Адрес самой почты и имя отправителя
+    $mail->setFrom('sergey.student1994@mail.ru', 'Сергей Кузьмичев'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
-    $mail->addAddress('');  
+    $mail->addAddress('drumer19940310@mail.ru');  
 
     // Отправка сообщения
     $mail->isHTML(true);
     $mail->Subject = $title;
     $mail->Body = $body;    
+    $mail->Body = $message;    
 
 // Проверяем отравленность сообщения
 if ($mail->send()) {$result = "success";} 
@@ -60,4 +105,4 @@ else {$result = "error";}
 }
 
 // Отображение результата
-{header('location: index.html');
+header('location: index.html');
